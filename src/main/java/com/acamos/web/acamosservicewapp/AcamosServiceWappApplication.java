68c28757e1ffa3d177683a889fb36e8f7e616f07
Acamos Tech <122689420+acamostech@users.noreply.github.com>
2023-01-22 21:28:50 -0500

    update the code with maven

---
+++ src/main/java/com/acamos/web/acamosservicewapp/AcamosServiceWappApplication.java
@@ -0,0 +1,13 @@
+package com.acamos.web.acamosservicewapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcamosServiceWappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcamosServiceWappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acamos/web/acamosservicewapp/AcamosServiceWappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acamos.web.acamosservicewapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcamosServiceWappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
